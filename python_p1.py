8883c81d117bfab474a30a731c58c423495042c1
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-08-17 22:33:40 +0530

    python_p1.py

--- python_p1.py
+++ python_p1.py
@@ -1,4 +1,4 @@
 #Inches to cm
 inches=float(input("Enter the value in inches: "))
 cm=2.54*inches
-print("{} inches = {:.2f} cm".format(inches,cm))
+print("{:.2f} inches = {:.2f} cm".format(inches,cm))

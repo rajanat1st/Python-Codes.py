160514c3288b5aa29181778abffbbf802ecfb13d
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-08-21 00:37:25 +0530

    python_p4.py

--- python_p4.py
+++ python_p4.py
@@ -1,1 +1,9 @@
-print("hi")
+#Program to find average of n numbers
+n=int(input("Enter the number of elements you want to enter: "))
+print("Enter",n,"numbers: ")
+lst=[]
+for i in range(n):
+    elem=int(input())
+    lst.append(elem)
+avg=sum(lst)/n
+print("Average = {:.2f}".format(avg))

8165e4a1a2b43fd7df2f583782c0e55091614253
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-08-23 23:07:23 +0530

    Add files via upload

---
+++ python_p7.py
@@ -0,0 +1,9 @@
+# Write a program to print all arithmetic operations on two given numbers
+def arithmetic_operations(m,n):
+    print("{}+{}={}".format(m,n,m+n))
+    print("{}-{}={}".format(m,n,m-n))
+    print("{}x{}={}".format(m,n,m*n))
+    print("{}/{}={:.2f}".format(m,n,m/n))
+m,n=map(int,input("Enter any two numbers: ").split())
+arithmetic_operations(m,n)
+    

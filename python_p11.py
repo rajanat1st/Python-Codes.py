6cc89f7df22b9ccc4fcdf7a09d24b60c125674f3
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-08-27 16:37:13 +0530

    Add files via upload

---
+++ python_p11.py
@@ -0,0 +1,5 @@
+# Write a program to compute the area of a triangle by reading base and height
+b=float(input("Enter the base of triangle: "))
+h=float(input("Enter the height of triangle: "))
+a=.5*b*h
+print("Area = {:.2f} square units".format(a))

b5f4a1183c59ee81d75b8506bfe2c63748997d49
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-08-30 17:20:48 +0530

    Add files via upload

---
+++ python_p14.py
@@ -0,0 +1,5 @@
+# Write a python program to swap the values of 'a' and 'b' without using third variable
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+a,b=b,a
+print("a =",a,"\nb =",b)

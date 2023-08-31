3b91ed153ee54d1f7a6151683b2a68ea20b0db67
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-08-31 22:04:01 +0530

    Add files via upload

---
+++ python_p15.py
@@ -0,0 +1,6 @@
+# Write program to calculate the area and perimeter of a circle by reading the radius
+from math import pi
+r=float(input("Enter the radius of the circle: "))
+a=pi*r*r
+p=2*pi*r
+print("Area = {:.2f}  square units\nPerimeter = {:.2f} units".format(a,p))

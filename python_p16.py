06d5da4a660198faccc0f4650d5b2837178075c7
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-09-01 22:36:38 +0530

    Add files via upload

---
+++ python_p16.py
@@ -0,0 +1,7 @@
+'''Romeo has X 5 rupee coins and Y 10 rupee coins. Romeo goes to a shop to buy chocolates for Juliet where each chocolate costs
+Z rupees. Find tne maximum number of chocolates that Romeo can buy for Juliet (Take X, Y and Z from the console).'''
+x=int(input("Enter the value of X: "))
+y=int(input("Enter the value of Y: "))
+z=int(input("Enter the value of Z: "))
+c=((x*5)+(y*10))//z
+print(" The maximum chocolates Romeo can buy =",c)

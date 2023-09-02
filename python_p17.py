d614c6b8b94f6022338a7188ed1f7d209312bf48
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-09-02 22:36:40 +0530

    Add files via upload

---
+++ python_p17.py
@@ -0,0 +1,5 @@
+# Write a program to converte given minutes into hours and minutes.
+min=int(input("Enter the number of minutes: "))
+h=min//60
+m=min%60
+print(min,"minute(s) =",h,"hour(s) and",m,"minute(s)")

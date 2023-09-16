ce01746fc394c4ecc4263b160881f1448769220a
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-09-16 19:26:46 +0530

    Add files via upload

---
+++ Climate.py
@@ -0,0 +1,8 @@
+''' Mr.Snowman considers the climate HOT if the temperature is above 20, otherwise he considers it COLD. You are given the
+temperature C, find whether the climate is HOT or COLD. '''
+
+c=int(input("Enter the temperature in celsius: "))
+if c>20:
+    print("Climate is HOT now")
+else:
+    print("Climate is COLD now")

aa280f46ff4832201779e10b2d07521366d683cd
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-09-18 20:12:52 +0530

    Add files via upload

---
+++ Number_Divisible_with_9_or_Not.py
@@ -0,0 +1,8 @@
+''' Write a program to check whether the given number is divisible with 9 or not? Print 'True' if divisible and 'False'
+if not divisible. '''
+
+n=int(input("Enter a number: "))
+if n%9==0:
+    print("\nTrue")
+else:
+    print("\nFalse")

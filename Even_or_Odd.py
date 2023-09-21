da4b509e5859b200841449bda9b1f2f646d22ff1
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-09-21 22:05:49 +0530

    Add files via upload

---
+++ Even_or_Odd.py
@@ -0,0 +1,6 @@
+# Write a program to find whether a number is Even or Odd.
+n=int(input("Enter a number: "))
+if n%2==0:
+    print(f"\n{n} is an Even number")
+else:
+    print(f"\n{n} is an Odd number")

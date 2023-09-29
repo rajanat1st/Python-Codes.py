fd77c1f566bed60f48fc4c1f6f4f150c78c98162
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-09-29 22:16:04 +0530

    Add files via upload

---
+++ Pattern_Equilateral_Triangle.py
@@ -0,0 +1,8 @@
+# Print Equilateral Triangle pattern with stars.
+n=int(input("Enter the number of Rows: "))
+for i in range(1,n+1):
+    for j in range(1,n-i+1):
+        print(" ",end="")
+    for j in range(1,i+1):
+        print("* ",end="")
+    print()

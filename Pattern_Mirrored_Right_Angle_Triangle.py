2bee9b5433f978d017658e7a8ddd66656593f76c
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-09-30 12:49:27 +0530

    Pattern_Mirrored_Right_Angle_Triangle.py

---
+++ Pattern_Mirrored_Right_Angle_Triangle.py
@@ -0,0 +1,10 @@
+# Print Mirrored Right angle Triangle pattern with stars.
+n=int(input("Enter the number of Rows: "))
+for i in range(1,n+1):
+    # i^th row has (n-i) leading spaces
+    for j in range(1,n-i+1):
+        print("  ",end="")
+    # i^th row has i elements
+    for j in range(1,i+1):
+        print("* ",end="")
+    print()

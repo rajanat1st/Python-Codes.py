ed07a86263784dcf5912e22b2e2a3bbaecf830d8
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-10-04 22:59:10 +0530

    Add files via upload

---
+++ Alphabets_using_ASCII_Values.py
@@ -0,0 +1,9 @@
+# Program to print Alphabets using ASCII Values.
+print("Alphabets from A to Z: ")
+for i in range(65,91):
+    print(chr(i),end=" ")
+print("\n")
+print("Alphabets from a to z: ")
+for i in range(97,123):
+    print(chr(i),end=" ")
+    

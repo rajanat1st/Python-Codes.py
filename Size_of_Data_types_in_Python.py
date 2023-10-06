5db92d3bf4ae1884aecc39b4ea811005f7e53588
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-10-06 23:50:58 +0530

    Add files via upload

---
+++ Size_of_Data_types_in_Python.py
@@ -0,0 +1,9 @@
+# Write a Python progrm to find the Size of data types in python.
+import sys
+print("The size of int is",sys.getsizeof(int()))
+print("The size of float is",sys.getsizeof(float()))
+print("The size of string is",sys.getsizeof(str()))
+print("The size of list is",sys.getsizeof(list()))
+print("The size of set is",sys.getsizeof(set()))
+print("The size of tuple is",sys.getsizeof(tuple()))
+print("The size of Dictionary is",sys.getsizeof(dict()))

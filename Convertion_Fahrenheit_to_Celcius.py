c7a39b8c375cb0d5b49fcda2840c4035b2faa121
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-10-07 23:39:07 +0530

    Convertion_Fahrenheit_to_Celsius.py

--- Convertion_Fahrenheit_to_Celcius.py
+++
@@ -1,4 +0,0 @@
-# Program to convert Fahrenheit to Celcius.
-f=float(input("Enter Temperature in Fahrenheit: "))
-c=(f-32)*5/9
-print("Temperature in Degree Celcius = {:.2f}".format(c))
---
+++ Convertion_Fahrenheit_to_Celsius.py
@@ -0,0 +1,4 @@
+# Program to convert Fahrenheit to Celsius.
+f=float(input("Enter Temperature in Fahrenheit: "))
+c=(f-32)*5/9
+print("Temperature in Degree Celsius = {:.2f}".format(c))

f03dc45dea1d005deedb764ac5efc36aaff39e11
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-12-11 06:00:51 +0530

    Add files via upload

---
+++ Print_Sum_Diff_Product.py
@@ -0,0 +1,8 @@
+''' The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers. '''
+a = int(input('Enter the value of a:'))
+b = int(input('Enter the value of b:'))
+print("Sum= {}\nDifference= {}\nProduct= {}".format(a+b, a-b, a*b))

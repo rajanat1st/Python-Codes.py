1e9ea94c154f81440bf3e57ad5251683cbd724fb
rajanat1st <142513338+rajanat1st@users.noreply.github.com>
2023-12-14 05:26:35 +0530

    Add files via upload

---
+++ intAndFloorDivision.py
@@ -0,0 +1,5 @@
+''' Read a and b from the console. Add logic to print two lines. The first line should contain the result of integer division, a//b .
+The second line should contain the result of float division, a/b . '''
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of a: "))
+print("By integer divsion: ",a//b,"\nBy float division: ",a/b)
